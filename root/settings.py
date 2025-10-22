4a53be1bba5d4dbc71df4d4c9345a981608d49c2
Abdulaziz <abdulazizmirobidov75@gmail.com>
2025-10-22 20:11:56 +0500

    CSRF_TRUSTED_ORIGINS link

--- root/settings.py
+++ root/settings.py
@@ -37,7 +37,8 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
 
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://7c382665d1e6.ngrok-free.app"
+    "https://7c382665d1e6.ngrok-free.app",
+    "https://a-u3w2.onrender.com"
 ]
 
 
